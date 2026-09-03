affd42d3be558cd6744810a082d6723f09fe2a66
deviprasad716 <deviprasadkalmalemath@gmail.com>
2026-09-03 22:44:46 +0530

    Delete project-1_WeightConverter.py

--- project-1_WeightConverter.py
+++
@@ -1,9 +0,0 @@
-weight=float(input("Enter your weight: "))
-unit=input("(L)bs or (K)g: ")
-
-if unit.lower()=='l':
-    converted_weight=weight*0.45
-    print(f"You are {converted_weight} kgs..")
-else:
-    converted_weight=weight/0.45
-    print(f"You are {converted_weight} pounds..")
